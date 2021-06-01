4dc77e1060eca4d1d9ffec25ac54934ce8b6948d
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-06-01 07:16:53 +0000

    Add files via upload

---
+++ CoursPytorch/CIFAR10/models/resnet/conv3_3.py
@@ -0,0 +1,3 @@
+def conv3_3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3, 
+                     stride=stride, padding=1, bias=False)
---
+++ CoursPytorch/CIFAR10/models/resnet/residual_block.py
@@ -0,0 +1,22 @@
+class block_residuel(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(block_residuel, self).__init__()
+        self.conv1 = conv3_3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3_3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
